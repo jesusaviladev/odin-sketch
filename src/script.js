c3ef782d4ffc855fad4521571ccabd8bc52ec10f
Jesus Avila <avijesus13@gmail.com>
2024-08-21 13:54:33 -0400

    finished grid

---
+++ src/script.js
@@ -0,0 +1,50 @@
+const canvas = document.querySelector("#container");
+const button = document.querySelector("#new");
+const reset = document.querySelector("#reset");
+
+const GRID_WIDTH = 640;
+
+const renderGrid = (gridSize = 16) => {
+  const grid = new Array(gridSize)
+    .fill(0)
+    .map(() => new Array(gridSize).fill(0));
+
+  grid.forEach((row) => {
+    row.forEach(() => {
+      const box = document.createElement("div");
+      box.setAttribute("class", "box");
+      box.textContent = "";
+
+      if (gridSize !== 16) {
+        box.style.width = `${GRID_WIDTH / gridSize}px`;
+        box.style.height = `${GRID_WIDTH / gridSize}px`;
+      }
+
+      canvas.appendChild(box);
+    });
+  });
+};
+
+const removeGrid = () => {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+};
+
+button.addEventListener("click", () => {
+  const gridSize = Number(prompt("Enter a number between 2 and 100"));
+
+  if (gridSize < 2 || gridSize > 100 || Number.isNaN(gridSize)) {
+    throw new Error("Invalid grid size");
+  }
+
+  removeGrid();
+  renderGrid(gridSize);
+});
+
+reset.addEventListener("click", () => {
+  removeGrid();
+  renderGrid();
+});
+
+renderGrid();
